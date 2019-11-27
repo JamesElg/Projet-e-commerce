f3346623aa0e76a4e8d70508c77b4f2e56335b87
Nathan <nathancpt1@gmail.com>
2019-11-27 19:09:08 +0100

    STRIPE MARCHE

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,5 @@ Rails.application.routes.draw do
   root to: "items#index"
   resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :mon_panier, only: [:index]
 end
