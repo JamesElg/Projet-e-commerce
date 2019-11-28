16853287cfab37f9284f8e8bd427b75104af7905
Nathan <nathancpt1@gmail.com>
2019-11-28 19:02:51 +0100

    add mon_profil

--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,5 @@ Rails.application.routes.draw do
   resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :mon_panier, only: [:index]
+  resources :mon_profil, only: [:index]
 end
